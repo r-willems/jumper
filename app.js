5f6797508598fbea9fe60ef1c501c683ade17fbb
r-willems <roelofwillems@gmail.com>
2023-07-19 22:05:22 +0200

    javascript mode

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 //git checkout main||advanced om naar andere versie te gaan
 
-let container = document.getElementById("gameBox");
+let gameBoard = document.getElementById("gameBox");
 
-let block = document.getElementById("block");
+let nmySpd = 1;
 
-let loop = false;
-
-let spriteBt;
-let spriteLt;
-let spriteRt;
-
-let blockLt;
-let blockRt;
-
-let statPts = document.getElementsByClassName("points");
-let statLvs = document.getElementsByClassName("lives");
 let points = 0;
 let lives = 6;
+let hit = false;
 
 let game = null;
 
-const ctx = container.getContext("2d");
+const ctx = gameBoard.getContext("2d");
 const sprite = {
     x: 50,
     y: 120,
@@ -31,14 +21,25 @@ const sprite = {
 };
 
 const nemesis = {
-    x: 288,
+    x: 300,
     y: 135,
     width: 12,
     height: 15,
     speed: 5
 };
 
-//drawSprite();
+
+document.addEventListener("keydown", stop=>{
+    if(stop.code === "Enter" && nmySpd != 0)
+    {
+        nmySpd = 0;
+        console.log(`Nmy: Y${nemesis.y}, X${nemesis.x}`);
+    }
+    else if (stop.code === "Enter" && nmySpd == 0)
+    {
+        nmySpd = 1;
+    }
+});
 
 document.addEventListener("keyup", moveSprite);
 document.addEventListener("keydown", keyD =>{
@@ -50,37 +51,64 @@ document.addEventListener("keydown", keyD =>{
 
 
 
-function drawSprites()
-{
-    ctx.clearRect(0, 0, container.width, container.height);
-    ctx.fillStyle = "blue";
-    ctx.fillRect(sprite.x, sprite.y, sprite.width, sprite.height);
 
-    
+function checkHit()
+{
+    if(!hit && (nemesis.x-12) <= sprite.x && nemesis.x >= (sprite.x-12) && (sprite.y+30) >= 150 - nemesis.height)
+    {
+       console.log("BOOM!");
+       hit = true
+    }
+    else if (!hit && nemesis.x == 0)
+    {
+        points++;
+        console.log(`POINTS: ${points}`);
+        hit = false;
+    }
+    else if (hit && nemesis.x == 0)
+    {
+        lives--;
+        console.log(`LIVES ${lives}`);
+        hit = false;
+    }
+
 }
+
+
+
 
 function drawNemesis()
 {
     ctx.fillStyle = "red";
     ctx.fillRect(nemesis.x, nemesis.y, nemesis.width, nemesis.height);
 }
-
-
 function moveNemesis()
 {
-    setInterval( ()=>{
+    nemesis.x -= nmySpd;
+    
 
-        nemesis.x--;
-    }, 2000);
-    drawNemesis();
+    if (nemesis.x == 0)
+    {
+        setTimeout( ()=>{
+            nemesis.x = 312;
+        }, 1000);
+        
+    } 
 
 }
 
+function drawSprite()
+{
+    ctx.clearRect(0, 0, gameBoard.width, gameBoard.height);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(sprite.x, sprite.y, sprite.width, sprite.height);
 
-
+}
 function moveSprite(event)
 {
-    //console.log(container.width, "|", container.height);
+    //console.log(gameBoard.width, "|", gameBoard.height);
+    let jump = false;
+    //Even naar kijken dat sprite door de bodem kan door tijdens jump op omlaag te drukken
     
     switch (event.code)
     {
@@ -88,22 +116,26 @@ function moveSprite(event)
             sprite.y -= sprite.jump;
             setTimeout(() =>{
                 sprite.y += sprite.jump;
+                
             }, 600);
+            //console.log(jump);
             if (sprite.y <= 0)
             {
                 sprite.y = 0;
                 break;
             }
-        break;
-
+            
+            break;
+        
         case "ArrowDown":
+
             sprite.y += sprite.speed;
             if (sprite.y >= 120)
             {
                 sprite.y = 120;
                 break;
             }
-        break;
+            break;
 
         case "ArrowLeft":
             sprite.x -= sprite.speed;
@@ -126,19 +158,20 @@ function moveSprite(event)
         default:
             break;
     }
-    console.log(sprite);
-
+    console.log(`Spr: Y${sprite.y}, X${sprite.x}`);
     
 }
 
-function gameLoop() {
-    drawSprites();
+function gameLoop() 
+{
+    drawSprite();
     drawNemesis();
-
     moveNemesis();
+
+    checkHit();
 
 
     requestAnimationFrame(gameLoop);
-  }
+}
   
-  gameLoop();
+gameLoop();
